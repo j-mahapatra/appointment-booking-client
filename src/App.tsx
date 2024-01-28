a7f3f99c1d270afe5209810b32a46fb1b421b23e
j-mahapatra <jagannathm.official@gmail.com>
2024-01-29 03:27:04 +0530

    feat: setup login and register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Signin from './pages/Signin';
+import Login from './pages/Login';
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div className='bg-secondary h-screen'>
+      <BrowserRouter>
+        <h1 className='text-primary text-center p-5 font-semibold text-3xl'>
+          Appointment Booking
+        </h1>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Signin />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
