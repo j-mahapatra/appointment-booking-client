44b22b42eee0d4a21c9044ecc831a1d94f5a6276
j-mahapatra <jagannathm.official@gmail.com>
2024-01-29 10:38:03 +0530

    feat: add slot booking feature for sales team

---
+++ src/components/SlotBook.tsx
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+
+export default function SlotBook({
+  physioData,
+  selectedPhysio,
+  selectedDay,
+  sectionOfDay,
+}: {
+  physioData: UserType[];
+  selectedPhysio: string;
+  selectedDay: string;
+  sectionOfDay: string;
+}) {
+  const [currentPhysio, setCurrentPhysio] = useState<UserType>();
+  const [bookedSlots, setBookedSlots] = useState<string[]>([]);
+
+  useEffect(() => {
+    const current = physioData.filter(
+      (physio) => physio.email === selectedPhysio
+    )?.[0];
+    setCurrentPhysio(current);
+  }, [physioData, selectedPhysio]);
+
+  const bookTimeSlot = async (slotId: string, slot: string) => {
+    await axios
+      .patch(`${import.meta.env.VITE_SERVER_URL}/api/slots/book`, {
+        slotId,
+      })
+      .then(() => {
+        setBookedSlots((prev) => [...prev, slot]);
+        toast.success('Appointment slot booked.');
+      })
+      .catch(() => {
+        toast.error('Appointment booking failed');
+      });
+  };
+
+  const getDaySection = (slot: string): string => {
+    const hours = parseInt(slot.substring(0, 2));
+
+    if (hours < 12) {
+      return 'morning';
+    } else if (hours < 16) {
+      return 'afternoon';
+    } else {
+      return 'evening';
+    }
+  };
+
+  return (
+    <div className='flex flex-col w-full items-center justify-center my-5'>
+      <p className='text-center'>Click on a slot to book:</p>
+      <div>
+        {currentPhysio?.free_slots?.map(
+          (slot: SlotType) =>
+            slot.day === selectedDay &&
+            getDaySection(slot.slot) === sectionOfDay &&
+            !slot.isBooked && (
+              <button
+                key={slot._id}
+                onClick={() => bookTimeSlot(slot._id, slot.slot)}
+                disabled={bookedSlots?.includes(slot.slot)}
+                className={`flex m-2 justify-center rounded-md border border-primary p-2 w-24 ${
+                  bookedSlots?.includes(slot.slot) && 'bg-gray-700'
+                }`}
+              >
+                {slot.slot}
+              </button>
+            )
+        )}
+      </div>
+    </div>
+  );
+}
--- src/pages/partials/SalesView.tsx
+++ src/pages/partials/SalesView.tsx
@@ -1,3 +1,107 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import SlotBook from '../../components/SlotBook';
+
 export default function SalesView() {
-  return <div className='text-white'>SalesView</div>;
+  const [physioData, setPhysioData] = useState<UserType[]>([]);
+  const [selectedPhysio, setSelectedPhysio] = useState<string>('');
+  const [day, setDay] = useState<string>('');
+  const [daySection, setDaySection] = useState<string>('');
+
+  useEffect(() => {
+    const getPhysioData = async () => {
+      const users = await axios.get(
+        `${import.meta.env.VITE_SERVER_URL}/api/user/get-physios`
+      );
+      setPhysioData(users.data.data);
+    };
+    getPhysioData();
+  }, []);
+
+  return (
+    <div className='flex flex-col space-y-5 w-full justify-center items-center text-white p-5'>
+      <div className='flex flex-col sm:flex-row items-center space-x-2'>
+        <select
+          name='physio'
+          id='physio'
+          value={selectedPhysio}
+          onChange={(e) => setSelectedPhysio(e.target.value)}
+          className='bg-transparent outline-none'
+        >
+          <option disabled={true} value='' className='text-gray-700'>
+            Select a Physiotherapist
+          </option>
+          {physioData?.map((physio) => (
+            <option
+              key={physio._id}
+              value={physio.email}
+              className='text-black'
+            >
+              {physio.email}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className='flex justify-center space-x-2'>
+        <select
+          name='day'
+          id='day'
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+          className='bg-transparent outline-none'
+        >
+          <option disabled={true} value='' className='text-gray-700'>
+            Select a day
+          </option>
+          <option value='monday' className='text-black'>
+            Monday
+          </option>
+          <option value='tuesday' className='text-black'>
+            Tuesday
+          </option>
+          <option value='wednesday' className='text-black'>
+            Wednesday
+          </option>
+          <option value='thursday' className='text-black'>
+            Thursday
+          </option>
+          <option value='friday' className='text-black'>
+            Friday
+          </option>
+          <option value='saturday' className='text-black'>
+            Saturday
+          </option>
+        </select>
+      </div>
+      <div className='flex justify-center space-x-2'>
+        <select
+          name='day'
+          id='day'
+          value={daySection}
+          onChange={(e) => setDaySection(e.target.value)}
+          className='bg-transparent outline-none'
+        >
+          <option disabled={true} value='' className='text-gray-700'>
+            Select section of day
+          </option>
+          <option value='morning' className='text-black'>
+            Morning
+          </option>
+          <option value='afternoon' className='text-black'>
+            Afternoon
+          </option>
+          <option value='evening' className='text-black'>
+            Evening
+          </option>
+        </select>
+      </div>
+      <SlotBook
+        physioData={physioData}
+        selectedPhysio={selectedPhysio}
+        selectedDay={day}
+        sectionOfDay={daySection}
+      />
+    </div>
+  );
 }
